06980caf5ee2e6dd7b9c463c5cac0424d3fdfd2c
JeongYeon <cosmos8749@naver.com>
2019-06-20 23:27:12 +0900

    190620 rule 클래스 작성

--- javagame/src/javagame/kaeubaeubo.java
+++ javagame/src/javagame/kaeubaeubo.java
@@ -4,7 +4,40 @@ import java.util.*;
 
 /*
  * overriding vs overloading
+ * 
+ * overriding: 하위클래스가 부모의 동작방법을 변경
+ * 
  */
+
+class rule {
+	
+	String kababo = ""; 
+	
+	public void kor(int k) {
+		
+		switch (k) {
+		case 1:kababo = "가위"; break;
+		case 2:kababo = "바위"; break;
+		case 3:kababo = "보"; break;
+		default:kababo ="";
+			break;
+		}
+		
+		System.out.println(kababo + "냈음.");
+	}
+	
+	
+	//계산후에 승자 리턴
+	public int winner(int u,int i) {
+		
+		
+		
+		return 0;
+	}
+}
+
+//셋이서 가위바위보
+
 public class kaeubaeubo {
 
 	public static void main(String[] args) {
@@ -19,15 +52,8 @@ public class kaeubaeubo {
 		double rv = Math.random();
 		int iv = (int)(rv * 3) + 1; //1~3출력
 		
-		String kababo = ""; 
 		
-		switch (uv) {
-		case 1:kababo = "가위"; break;
-		case 2:kababo = "바위"; break;
-		case 3:kababo = "보"; break;
-		default:kababo ="";
-			break;
-		}
+		
 		
 		
 		

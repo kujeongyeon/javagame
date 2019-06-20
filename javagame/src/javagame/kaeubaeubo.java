6d6ea4735b3afff3a932ac2e63793599e9e9f628
JeongYeon <cosmos8749@naver.com>
2019-06-20 23:50:18 +0900

    190620 rule 클래스 완성

--- javagame/src/javagame/kaeubaeubo.java
+++ javagame/src/javagame/kaeubaeubo.java
@@ -30,7 +30,24 @@ class rule {
 	//계산후에 승자 리턴
 	public int winner(int u,int i) {
 		
+		String sum = u+i+"";
 		
+		if(u == i) {
+			return 0;
+		}
+		else 
+		{
+			if(sum.equals("3"))
+			{
+				return 2;
+			}else if(sum.equals("4")) 
+			{
+				return 3;
+			}else if(sum.equals("5")) 
+			{
+				return 3;
+			}
+		}
 		
 		return 0;
 	}
@@ -41,10 +58,14 @@ class rule {
 public class kaeubaeubo {
 
 	public static void main(String[] args) {
-		
+
+		rule ru = new rule();
 		
 		Scanner sc = new Scanner(System.in);
 		int uv = sc.nextInt();
+		
+		ru.kor(uv);
+		
 		
 		Random r = new Random();
 		
@@ -52,11 +73,14 @@ public class kaeubaeubo {
 		double rv = Math.random();
 		int iv = (int)(rv * 3) + 1; //1~3출력
 		
+		ru.kor(iv);
 		
-		
-		
-		
-		
+		int num = ru.winner(uv,iv);
+		if(uv == num) {
+			System.out.println("당신이 이겼습니다.");
+		}else {
+			System.out.println("컴퓨터가 이겼습니다.");
+		}
 		
 		
 	}
